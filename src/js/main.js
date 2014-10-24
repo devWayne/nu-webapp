1aa13aa6ec3166435eb50e227a3a9abf059ba924
tianwen.zhou <tianwen.zhou@dianping.com>
2014-10-24 18:01:20 +0800

    更新

--- src/js/main.js
+++ src/js/main.js
@@ -21,12 +21,20 @@ define(function(require, exports, module) {
 		appLongitude = httpUtils.getParam('longitude');
         couponRuleID = httpUtils.getParam('couponRuleID');
         env = httpUtils.getParam('env');
-	var params = "eventName=" + eventName + "&cityid=" + cityid + "&version=" + version + "&token=" + token + "&latitude=" + appLatitude + "&longitude=" + appLongitude + "&couponRuleID=" + couponRuleID;
+	var params = "eventName=" + eventName + "&cityid=" + cityid + "&version=" + version + "&token=" + token + "&latitude=" + appLatitude + "&longitude=" + appLongitude + "&dpid=" + dpid + "&couponRuleID=" + couponRuleID;
+	var loginParams = "eventName=" + eventName + "&cityid=" + cityid + "&version=" + version + "&token=*" + "&latitude=" + appLatitude + "&longitude=" + appLongitude + "&dpid=" + dpid + "&couponRuleID=" + couponRuleID;
 
-	var mdomain='http://m.51ping.com';
-    var dealInfoUrl = 'http://evt.dianping.com/mfchwl/json/1.json';
+
+	var mdomain='http://m.dianping.com';
+	var eDomain='http://t.dianping.com';
+    var userInfoUrl = 'http://mm.dianping.com/weixin/promotion/user-profile/jsonp';
+    var dealInfoUrl = 'http://evt.dianping.com/mfchwl/json/'+cityid+'.json';
+
     if(env != 'product'){
-        dealInfoUrl = 'http://evt.dianping.dp/mfchwlzs/json/1.json';
+        dealInfoUrl = 'http://evt.dianping.dp/mfchwlzs/json/'+cityid+'.json';
+        userInfoUrl = 'http://mm.51ping.com/weixin/promotion/user-profile/jsonp';
+        mdomain='http://m.51ping.com';
+        eDomain='http://t.51ping.com';
     }
 	var _g=new geo();
 	var nList=1,perList=1;
@@ -109,6 +117,8 @@ define(function(require, exports, module) {
 	function getUserInfo() {
 
 		var data = {};
+        data.couponRuleID = couponRuleID;
+        data.dpid = dpid;
 		if ($.cookie('dper')) {
 			data.dper = $.cookie('dper');
 		} else {
@@ -117,18 +127,18 @@ define(function(require, exports, module) {
 
 		$.ajax({
 			async: false,
-			url: 'http://mm.51ping.com/weixin/promotion/user-profile/jsonp',
+			url: userInfoUrl,
 			type: "GET",
 			dataType: 'jsonp',
 			data: data,
 			jsonp: 'jsonp',
 			success: function(json) {
-			if(json.success&&json.realtime_tg_fresh){
-				$('.buy-btn').text('免费领');
-				$('.price strong').text('0')
+			if(json.success&&json.realtime_tg_fresh == 0){
+                $('.container.op-box').append('<img src="http://t.dianping.com/events/m/mfchgds/lyhneww.png">')
 			}
 			else{
-			$('.container.op-box').append('<img src="http://t.dianping.com/events/m/mfchgds/lyhneww.png">')
+                $('.buy-btn').text('免费领');
+                $('.price strong').text('0')
 			}
 				setDirect(json);		
 			}
@@ -143,10 +153,8 @@ define(function(require, exports, module) {
 			var dealId = $(e.target).parents('a').attr('data-tuandealid');
 
 			var dealUrl = mdomain+"/tuan/eventdeal/" + dealId+ '?' + params;
+			var loginDealUrl = mdomain+"/tuan/eventdeal/" + dealId+ '?' + loginParams;
 			var mDealUrl = mdomain+"/tuan/deal/" + dealId;
-            if(json.env == 'product'){
-               mdomain = 'http://m.dianping.com';
-            }
 
 			//status 1
 			if (version.replace(/\./, "") < 68.5) {
@@ -161,24 +169,28 @@ define(function(require, exports, module) {
 				return;
 			}
 			//status 2
-			if (!json.success) {
-				location.href = 'dianping://loginweb?url=' + encodeURIComponent(mdomain + '/login/app?version=' + version + '&logintype=m') + '&goto=' + encodeURIComponent('dianping://complexweb?url=' + encodeURIComponent(dealUrl));
+			if (json.success == 0) {
+				location.href = 'dianping://loginweb?url=' + encodeURIComponent(mdomain + '/login/app?version=' + version + '&logintype=m') + '&goto=' + encodeURIComponent('dianping://complexweb?url=' + encodeURIComponent(loginDealUrl));
+                return;
 			}
 
-            //oldUser  redirect to m.dianping.com site
-            if(json.realtime_tg_fresh != 1){
+            //oldUser redirect to m.dianping.com site
+            if(json.realtime_tg_fresh == 0){
                 location.href = mDealUrl;
+                return;
             }
 
 			//status 3
 			if (json.integrity_score <= 0) {
 			    $.removeCookie('dper');
-				location.href = 'dianping://loginweb?url=' + encodeURIComponent(mdomain + '/login/app?version=' + version + '&logintype=m') + '&goto=' + encodeURIComponent('dianping://complexweb?url=' + encodeURIComponent(dealUrl));
+				location.href = 'dianping://loginweb?url=' + encodeURIComponent(mdomain + '/login/app?version=' + version + '&logintype=m') + '&goto=' + encodeURIComponent('dianping://complexweb?url=' + encodeURIComponent(loginDealUrl));
+                return;
 			}
 
 			//status 4
 			if (!json.mobile) {
-				location.href = "dianping://modifyphone?goto=" + encodeURIComponent("dianping://complexweb?url=" + dealUrl) + '&url=' + encodeURIComponent(mdomain + "/account/modifyMobile?newtoken=!");
+				location.href = "dianping://modifyphone?goto=" + encodeURIComponent("dianping://complexweb?url=" + loginDealUrl) + '&url=' + encodeURIComponent(mdomain + "/account/modifyMobile?newtoken=!");
+                return;
 			}
 			//status 5
 			if (json.integrity_score > 9980) {
@@ -193,13 +205,12 @@ define(function(require, exports, module) {
 				return;
 			}
 			//status 6
-			if (json.integrity_score > 5000 && json.integrity_score < 9980) {
-				location.href = "/lab/common/uploadSmsCheck?eventId=" + eventId + "&bizType=2&callback=" + encodeURIComponent(dealUrl);
+			if (json.integrity_score > 5000 && json.integrity_score <= 9980 && json.up_sms == 1) {
+				location.href = eDomain + "/lab/common/uploadSmsCheck?eventId=" + couponRuleID + "&bizType=2&callback=" + encodeURIComponent(dealUrl);
+                return;
 			}
 			
 			location.href =dealUrl;
-			
-
 		});
 	}
 
